178ca7884a9d9b8b733f6f3b1e646df443cd4618
yewon-saurus <ywwwon01@gmail.com>
2024-12-02 22:14:54 +0900

    feat: room 마우스에 따라다니는 paddle 초안 작성

--- client/src/pages/room/[id].tsx
+++ client/src/pages/room/[id].tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import { Button } from "../components/Button";
 
@@ -13,15 +13,53 @@ const Room: NextPage<{}> = () => {
 
     const [isReady, setIsReady] = useState(false);
 
+    const hostStageRef = useRef<HTMLDivElement | null>(null);
+    const hostPaddleRef = useRef<HTMLDivElement | null>(null);
+    const guestStageRef = useRef<HTMLDivElement | null>(null);
+    const guestPaddleRef = useRef<HTMLDivElement | null>(null);
+
     useEffect(() => {
+        const hostStage = hostStageRef.current;
+        const hostPaddle = hostPaddleRef.current;
+        const guestStage = guestStageRef.current;
+        const guestPaddle = guestPaddleRef.current;
+
+        let stageRect: DOMRect | null = null;
+        let paddle: HTMLDivElement | null = null;
+
+        const handleMouseMove = (e) => {
+            let mouseX = e.clientX - stageRect!.left;
+            let mouseY = e.clientY - stageRect!.top;
+
+            const minX = 28;
+            const maxX = stageRect!.width - 28;
+            const minY = 28;
+            const maxY = stageRect!.height - 28;
+
+            mouseX = Math.max(minX, Math.min(maxX, mouseX));
+            mouseY = Math.max(minY, Math.min(maxY, mouseY));
+
+            paddle!.style.left = `${mouseX - 28}px`;
+            paddle!.style.top = `${mouseY - 28}px`;
+        }
+
         if (isHost === 'false') {
             setIsReady(true);
             socketInstance.emit('playerEntered', id);
+
+            stageRect = guestStage!.getBoundingClientRect();
+            paddle = guestPaddle;
         }
+        else {
+            stageRect = hostStage!.getBoundingClientRect();
+            paddle = hostPaddle;
+        }
+        window?.addEventListener("mousemove", handleMouseMove);
 
         socketInstance.on('roomReady', () => {
             setIsReady(true);
         });
+
     }, []);
 
     return (
@@ -39,11 +77,11 @@ const Room: NextPage<{}> = () => {
                 }
             </div>
             <div>
-                <div className="bg-red-100 h-[399px]">
-                    {/* TODO */}
+                <div ref={hostStageRef} className="bg-red-100 h-[399px] relative">
+                    <div ref={hostPaddleRef} className="w-14 h-14 bg-black border-white border rounded-full absolute pointer-events-none"></div>
                 </div>
-                <div className="bg-blue-100 h-[399px]">
-                    {/* TODO */}
+                <div ref={guestStageRef} className="bg-blue-100 h-[399px] relative">
+                    <div ref={guestPaddleRef} className="w-14 h-14 bg-white border-black border rounded-full absolute pointer-events-none"></div>
                 </div>
             </div>
         </>
